bf92c7ff09660edad6c0ea6fd883e812f223a103
Cesar Alexis Fajardo Flores <cesaralexisff07@gmail.com>
2019-07-22 13:06:58 -0500

    Creacion de server

---
+++ database.ts
@@ -0,0 +1,12 @@
+import mysql from 'promise-mysql';
+import keys from "./keys";
+
+//Conexion
+const db=mysql.createPool(keys.database);
+db.getConnection()
+    .then(connection=>{
+        db.releaseConnection(connection);
+        console.log('Base de datos conectada');
+    });
+
+    export default db;
